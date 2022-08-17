cccc97a4584c350b27ec6bdefd9628d29f1f506d
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-17 10:47:11 -0400

    refactored and modularized codebase

--- api.ts
+++ api.ts
@@ -1,19 +1,12 @@
-import express, {Request,Response} from "express"
+import express from "express"
 import cors from "cors"
-import {MongoClient,ObjectId} from "mongodb"
-import {uri} from "./credentials"
-const client = new MongoClient(uri)
-const db = client.db("BocaCode")
-const photosCollections = db.collection("photos")
+import { photoRouter  } from "./src/routers/photo-router"
 
-const app = express()
+const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.get("/",async (req: Request,res: Response)=> {
-    const photos = await photosCollections.find({}).toArray()
-    res.status(200).json(photos)
-})
+app.use('/photos', photoRouter)
 
 const PORT = 5001
 app.listen(PORT,()=> {
---
+++ src/db/db.ts
@@ -0,0 +1,8 @@
+import { MongoClient } from "mongodb";
+import {uri} from "../../credentials";
+const client = new MongoClient(uri)
+export const db = client.db("BocaCode")
+
+
+
+
